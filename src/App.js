170592eacf4a36e964672690a595e2b8c8d6ce52
daief48 <daief20103048@gmail.com>
2023-05-05 01:03:04 +0600

    add css and logics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [inputsList, setInputList] = useState("");
+  const [Items, setItems] = useState([]);
+
+  const itemEvents = (event) => {
+    setInputList(event.target.value);
+  }
+  const listOfItems = () => {
+    setItems((oldItems) => {
+      
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1>ToDo List</h1>
+          <br />
+          <input type="text" placeholder="Add a Item" onChange={itemEvents}/>
+          <button onClick={listOfItems}>+</button>
+
+          <ol>
+            <li>{inputsList}</li>
+          </ol>
+        </div>
+      </div>
+    </>
   );
 }
 
